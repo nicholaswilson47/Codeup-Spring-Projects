36b551818209b24020e4225347ddaf069fa9de1b
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-03-25 11:05:02 -0500

    Working on Dice roller

--- src/main/java/com/codeup/codeup_demo/DiceController.java
+++ src/main/java/com/codeup/codeup_demo/DiceController.java
@@ -1,2 +1,19 @@
-package com.codeup.codeup_demo;public class DiceController {
+package com.codeup.codeup_demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Controller
+public class DiceController {
+
+    @GetMapping("/roll-dice/{n}")
+    public String showRollDicePage(@PathVariable int n, Model viewModel){
+        int randomNum = ThreadLocalRandom.current().nextInt(1, 6+1);
+        viewModel.addAttribute("isCorrect", randomNum == n);
+        return"roll-dice";
+    }
 }
---
+++ src/main/java/com/codeup/codeup_demo/PostController.java
@@ -0,0 +1,7 @@
+package com.codeup.codeup_demo;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PostController {
+}
