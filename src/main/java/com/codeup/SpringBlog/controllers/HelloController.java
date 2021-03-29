39df4f0736079f64ffc97ec1bda53221465152fd
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-03-29 09:53:25 -0500

    updated homecontroller

--- src/main/java/com/codeup/SpringBlog/controllers/HelloController.java
+++ src/main/java/com/codeup/SpringBlog/controllers/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 class HelloController {
 
@@ -16,7 +19,17 @@ class HelloController {
     @GetMapping("/hello/{name}")
     @ResponseBody
     public String sayHello(@PathVariable String name, Model model) {
-        return "Hello" + name + "!";
+        List<String> names = new ArrayList<>();
+
+        names.add("Sam");
+        names.add("Dorian");
+        names.add("Diego");
+
+        model.addAttribute("aName", name.toUpperCase());
+        model.addAttribute("admin", name.equals("fer"));
+        model.addAttribute("nameList", names);
+
+        return "hello";
     }
     @GetMapping("/join")
     public String renderform(){
@@ -27,12 +40,7 @@ class HelloController {
             @RequestParam(name = "cohort") String cohort,
             Model viewModel){
         viewModel.addAttribute("welcomeMessage", "Welcome to" + cohort + "!");
+        return "join";
     }
 
-
-    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
-    @ResponseBody
-    public String addOne(@PathVariable int number){
-        return number + " plus one is " + (number + 1) + "!";
-    }
 }
