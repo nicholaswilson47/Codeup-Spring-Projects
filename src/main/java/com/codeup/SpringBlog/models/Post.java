1af2d0dfde31eb5b910957315f6cee90fc8e394b
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-03-29 10:30:51 -0500

    finished joinhtml

--- src/main/java/com/codeup/SpringBlog/models/Post.java
+++ src/main/java/com/codeup/SpringBlog/models/Post.java
@@ -1,5 +1,7 @@
 package com.codeup.SpringBlog.models;
 
+import javax.persistence.*;
+
 @Entity
 @Table(name ="posts" )
 public class Post {
--- src/main/java/com/codeup/SpringBlog/repo/PostRepo.java
+++ src/main/java/com/codeup/SpringBlog/repo/PostRepo.java
@@ -1,6 +1,6 @@
 package com.codeup.SpringBlog.repo;
 
-import com.codeup.springblog.models.Post;
+import com.codeup.SpringBlog.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
