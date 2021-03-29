7d35581c6f89633951ec0fba91a16b5bb067ef5b
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-03-29 10:43:16 -0500

    finished postjava

--- src/main/java/com/codeup/SpringBlog/models/Post.java
+++ src/main/java/com/codeup/SpringBlog/models/Post.java
@@ -16,5 +16,28 @@ public class Post {
     @Column(columnDefinition = "TEXT", length = 3000, nullable = false)
     private String body;
 
-
+    public Post(){
+    }
+    public Post(String title, String body){
+        this.title = title;
+        this.body = body;
+    }
+    public Long getId(){
+        return id:
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getBody(){
+        return body;
+    }
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
