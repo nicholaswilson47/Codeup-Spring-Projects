7397ca6d4ef8b94ddc13577b32af4e8ee18cf182
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-03-29 10:38:32 -0500

    started post.java

--- src/main/java/com/codeup/SpringBlog/models/Post.java
+++ src/main/java/com/codeup/SpringBlog/models/Post.java
@@ -5,4 +5,16 @@ import javax.persistence.*;
 @Entity
 @Table(name ="posts" )
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 200, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", length = 3000, nullable = false)
+    private String body;
+
+
 }
