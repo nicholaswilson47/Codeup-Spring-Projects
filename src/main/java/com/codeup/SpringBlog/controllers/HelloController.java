f00af8d6f1f9830b6a7f4973552c911cc336b0c0
Nicholas Wilson <nicholas.a.wilson47@gmail.com>
2021-03-29 09:46:39 -0500

    getmapping join on hello

--- src/main/java/com/codeup/SpringBlog/controllers/HelloController.java
+++ src/main/java/com/codeup/SpringBlog/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.SpringBlog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -14,8 +15,12 @@ class HelloController {
 
     @GetMapping("/hello/{name}")
     @ResponseBody
-    public String sayHello(@PathVariable String name) {
+    public String sayHello(@PathVariable String name, Model model) {
         return "Hello" + name + "!";
+    }
+    @GetMapping("/join")
+    public String renderform(){
+        return "join";
     }
 
     @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
